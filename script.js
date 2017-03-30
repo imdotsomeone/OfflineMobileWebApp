e1f6c53cb9f917b823d30abcc9c57975579d9c41
Dominik De Leon <niko_2he@yahoo.com>
2017-03-30 22:36:55 +0800

    java script for hiding and showing pages

---
+++ script.js
@@ -0,0 +1,34 @@
+function showorhidepage(id){
+    var divelement = document.getElementById(id);
+    divelement.classList.remove('hide');
+}
+
+var btn = document.getElementById("btn");
+var pageContainer = document.getElementById("page2");
+
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'JSONFile.json');
+    ourRequest.onload = function(){
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data){
+    var htmlString = "";
+    for(i=0; i < data.length; i++){
+        html += "<p>" + "Classcode: " + data[i].classcode + "<br>" +
+                "Title: " + data[i].title + "<br>" +
+                "Schedule: " + data[i].schedule + "<br>" +
+                "Classroom: " + data[i].classroom + "<br>" +
+                "Instructor: " + data[i].instructor + "</p>";
+    }
+    pageContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+var inputElement = document.getElementById("inputCode")
+
+
+/*svg script*/
